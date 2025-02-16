c9c226c816f73a5ed7d2905de5a8e246e543b0df
Dylan Zemlin <dylan.zemlin@ou.edu>
2025-02-16 08:10:56 -0600

    Refactor image receiving logic to use a dedicated function and improve socket handling

--- remote/image_processing.py
+++ remote/image_processing.py
@@ -1,16 +1,25 @@
-import socket
 import cv2
+import socket
+import pickle
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serversocket.bind(("", 5000))
+HOST = "127.0.0.1"
+PORT = 5000
 
-while True:
-    x = serversocket.recv(65536)
+def receive():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 65536)
+    sock.bind((HOST, PORT))
+    
+    while True:
+        data, addr = sock.recvfrom(65536)
+        frame_data = pickle.loads(data)
+        frame = cv2.imdecode(frame_data, cv2.IMREAD_COLOR)
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    
+    sock.close()
+    cv2.destroyAllWindows()
 
-    print(x)
-
-    cv2.Mat(x)
-
-    cv2.imshow("Frame", x)
-
-    raise SystemExit
+if __name__ == "__main__":
+    receive()

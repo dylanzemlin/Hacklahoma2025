16f510cc0dff74b72d10ffd053a93d2dc42c19c3
braden624 <bradenjames12@icloud.com>
2025-02-15 18:37:40 -0600

    Finished microphone.py, based off Rosie's microphone implementation.

--- onboard/microphone.py
+++ onboard/microphone.py
@@ -12,7 +12,22 @@ sock.sendall(type_message.encode())
 def record_callback(_, audio:sr.AudioData):
     global sock
 
+    data = audio.get_raw_data()
+    try:
+        sock.sendto(data (TCP_IP, TCP_PORT))
+    except Exception as e:
+        print(e)
+        try:
+            sock.close()
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((TCP_IP, TCP_PORT))
+            type_message = "MICROPHONE"
+            sock.sendall(type_message.encode())
+        except Exception as e:
+            print(e)
+
 def run():
+    
     recorder = sr.Recognizer()
     recorder.energy_threshold = 3200
     source = sr.Microphone(sample_rate=16000)
@@ -21,6 +36,13 @@ def run():
     
     recorder.listen_in_background(source, record_callback, phrase_time_limit=5)
 
+    while True:
+        try:
+            time.sleep(0.25)
+        except KeyboardInterrupt:
+            break
 
-if "__name__" == "__main__":
+
+if __name__ == "__main__":
+
     run()

2395800e9fe02f4f2e05c79ca581d58a2ee32bc5
braden624 <bradenjames12@icloud.com>
2025-02-15 18:35:35 -0600

    Changing more ways things are sent I don't know.

--- onboard/camera.py
+++ onboard/camera.py
@@ -10,18 +10,21 @@ def run():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((HOST, PORT))
     sock.listen(5)
+    vid = cv2.VideoCapture(0)
     while True:
         client, addr = sock.accept()
         print(f"Connection from {addr}")
         if client:
-            vid = cv2.VideoCapture(0)
+            vid.set(3, 1280)
+            vid.set(4, 720)
             while vid.isOpened():
                 img, frame = vid.read()
                 if not img:
+                    print("No image.")
                     break
                 data = pickle.dumps(frame)
-                size = len(data)
-                client.sendall(struct.pack(">L", size) + data)
+                client.sendall(struct.pack("Q", len(data) + data))
+    
 
 if __name__ == "__main__":
     run()
